fc47c4aa565b5e1e43bc1ce7f05ac6f0447e26b0
Ray Felton <frajeon@gmail.com>
2019-12-11 11:24:12 -0500

    'ayyyyyyy'

--- src/main/java/Security/Services/UserService.java
+++ src/main/java/Security/Services/UserService.java
@@ -6,16 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 
-import java.util.List;
-
+import java.nio.ByteBuffer;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
     public User save(User user){
-        return userRepository.save(user);
+        User user1 = new User();
+        user1.setFirst_name(user.getFirst_name());
+        user1.setLast_name(user.getLast_name());
+        user1.setEmail(user.getEmail());
+        user1.setPassword(user.getPassword());
+        user1.setId(ByteBuffer.wrap(KeyGenerators.secureRandom().generateKey()).getLong());
+        return userRepository.save(user1);
     }
     public User findByEmail(User user){
         User user1 = userRepository.findByEmail(user.getEmail());
--- src/main/java/Security/model/User.java
+++ src/main/java/Security/model/User.java
@@ -6,7 +6,6 @@ import javax.persistence.*;
 @Table(name = "user_boo")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String first_name;
     private String last_name;
