ef5374c459683950499a6a6ce259476962f459ee
Ray Felton <frajeon@gmail.com>
2019-12-11 10:34:50 -0500

    'ayyy

--- src/main/java/Security/Repository/UserRepository.java
+++ src/main/java/Security/Repository/UserRepository.java
@@ -1,9 +1,14 @@
 package Security.Repository;
 
 import Security.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends JpaRepository<User, Long> {
 
+public interface UserRepository extends JpaRepository<User, Long> {
+     User findByEmail(String email);
+
+     Page<User> findAll(Pageable pageable);
 }
--- src/main/java/Security/Services/UserService.java
+++ src/main/java/Security/Services/UserService.java
@@ -3,7 +3,11 @@ package Security.Services;
 import Security.Repository.UserRepository;
 import Security.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -12,5 +16,15 @@ public class UserService {
     UserRepository userRepository;
     public User save(User user){
         return userRepository.save(user);
+    }
+    public User findByEmail(User user){
+        User user1 = userRepository.findByEmail(user.getEmail());
+        if(user1.getPassword().equals(user.getPassword())){
+            return user1;
+        }
+        return null;
+    }
+    public Page<User> getAllUsersByPage(Pageable pageable){
+        return userRepository.findAll(pageable);
     }
 }
